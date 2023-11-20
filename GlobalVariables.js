f8463f5dab55b4927913889e75e305be64ab59e7
NormyRoe <124314202+NormyRoe@users.noreply.github.com>
2023-11-20 11:42:41 +1000

    Add files via upload

--- GlobalVariables.js
+++ GlobalVariables.js
@@ -6,12 +6,92 @@ export let competitionsData = [];
 
 export let selectedCompetitionId = null;
 
+export let divisionsData = [];
+
+export let selectedDivisionsId = null;
+
+export let usersData = [];
+
+export let selectedUserId = null;
+
+export let fieldsData = [];
+
+export let selectedFieldId = null;
+
+export let schoolsData = [];
+
+export let selectedSchoolsId = null;
+
+export let teamsData = [];
+
+export let selectedTeamsId = null;
+
+export let gameResultsData = [];
+
+export let selectedGameResultId = null;
+
+
 export const updateCompetitionsData = (data) => {
     competitionsData = data;
   };
 
-  export const updateSelectedCompetitionId = (id) => {
+export const updateSelectedCompetitionId = (id) => {
     selectedCompetitionId = id;
   };
 
+
+export const updateDivisionsData = (data) => {
+    competitionsData = data;
+  };
+
+export const updateSelectedDivisionsId = (id) => {
+    selectedCompetitionId = id;
+  };
+
+
+export const updateUsersData = (data) => {
+    competitionsData = data;
+  };
+
+export const updateSelectedUsersId = (id) => {
+    selectedCompetitionId = id;
+  };
+
+
+export const updateFieldsData = (data) => {
+    competitionsData = data;
+  };
+
+export const updateSelectedFieldsId = (id) => {
+    selectedCompetitionId = id;
+  };
+
+
+
+export const updateSchoolsData = (data) => {
+    competitionsData = data;
+  };
+
+export const updateSelectedSchoolsId = (id) => {
+    selectedCompetitionId = id;
+  };
+
+
+export const updateTeamsData = (data) => {
+    competitionsData = data;
+  };
+
+export const updateSelectedTeamsId = (id) => {
+    selectedCompetitionId = id;
+  };
+
+
+export const updateGameResultsData = (data) => {
+    competitionsData = data;
+  };
+
+export const updateSelectedGameResultsId = (id) => {
+    selectedCompetitionId = id;
+  };
   
+
