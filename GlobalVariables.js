d118a65a362d01bc3758a4fe389d7f69836bcb9a
NormyRoe <124314202+NormyRoe@users.noreply.github.com>
2023-11-27 11:15:33 +1000

    Add files via upload

--- GlobalVariables.js
+++ GlobalVariables.js
@@ -1,36 +1,36 @@
 // GlobalVariables.js
 
-export const BASE_URL = "http://192.168.1.106:8000";
+export const BASE_URL = "http://172.20.10.6:8000";
 
 export let competitionsData = [];
 
-export let selectedCompetitionId;
+export let selectedCompetitionId = 0;
 
 export let divisionsData = [];
 
-export let selectedDivisionId;
+export let selectedDivisionId = 0;
 
 export let usersData = [];
 
-export let selectedUserId;
+export let selectedUserId = 0;
 
 export let fieldsData = [];
 
-export let selectedFieldId;
+export let selectedFieldId = 0;
 
 export let schoolsData = [];
 
-export let selectedSchoolId;
+export let selectedSchoolId = 0;
 
 export let teamsData = [];
 
-export let selectedTeamId;
+export let selectedTeamId = 0;
 
 export let gameResultsData = [];
 
-export let selectedGameResultId;
+export let selectedGameResultId = 0;
 
-export let accessRole = null;
+export let accessRole = "";
 
 export let roundsData = [];
 
@@ -42,9 +42,6 @@ export const updateCompetitionsData = (data) => {
 export const updateSelectedCompetitionId = (id) => {
     selectedCompetitionId = id;
   };
-
-export const getSelectedCompetitionId = () => selectedCompetitionId;
-
 
 export const updateDivisionsData = (data) => {
     divisionsData = data;
